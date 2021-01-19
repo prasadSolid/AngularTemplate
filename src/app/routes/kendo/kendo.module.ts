6e7f4c73f0fd6b8b04fa33126e1d897d0fc91bc8
chianmsolidgit <46998808+chianmsolidgit@users.noreply.github.com>
2021-01-19 18:25:32 +0530

    kendo input elem.

--- src/app/routes/kendo/kendo.module.ts
+++ src/app/routes/kendo/kendo.module.ts
@@ -7,6 +7,14 @@ import { SchedulerComponent } from './scheduler/scheduler.component';
 import { DatagridComponent } from './datagrid/datagrid.component';
 import { DropdownsComponent } from './dropdowns/dropdowns.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { LabelModule } from '@progress/kendo-angular-label';
+
+
+
 const routes: Routes = [
     { path: '', component: InputComponent },
     { path: 'input', component: InputComponent },
@@ -21,6 +29,12 @@ const routes: Routes = [
   declarations: [InputComponent, ChartsComponent, SchedulerComponent, DatagridComponent, DropdownsComponent],
   imports: [
     CommonModule,
+    DateInputsModule,
+    DropDownsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LabelModule,
+
     RouterModule.forChild(routes),
   ],
   exports: [
