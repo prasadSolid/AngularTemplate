6a5928c7d4508d709d00e6a4c04d4dc59525b332
PrasadSolid <prasadd.soid@gmail.com>
2021-01-21 15:11:08 +0530

    Update datagrid.component.ts

--- src/app/routes/kendo/datagrid/datagrid.component.ts
+++ src/app/routes/kendo/datagrid/datagrid.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStateChangeEvent, GridDataResult } from '@progress/kendo-angular-grid/dist/es2015/main';
+import { process, State } from '@progress/kendo-data-query';
+import { sampleProducts } from './products';
 
 @Component({
   selector: 'app-datagrid',
@@ -7,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatagridComponent implements OnInit {
 
-  
+      public state: State = {
+        skip: 0,
+        take: 5,
+
+        // Initial filter descriptor
+        filter: {
+          logic: 'and',
+          filters: [{ field: 'ProductName', operator: 'contains', value: 'Chef' }]
+        }
+    };
+
+    public gridData: GridDataResult = process(sampleProducts, this.state);
+
+    public dataStateChange(state: DataStateChangeEvent): void {
+        this.state = state;
+        this.gridData = process(sampleProducts, this.state);
+    }
   constructor() { }
 
   ngOnInit(): void {
